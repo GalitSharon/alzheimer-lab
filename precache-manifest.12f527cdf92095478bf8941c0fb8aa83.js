2bd035af1c7a832c3b0bcde816c1131c24e493bb
galit67 <galitsharon67@gmail.com>
2021-05-23 23:09:08 +0300

    Updates

---
+++ precache-manifest.12f527cdf92095478bf8941c0fb8aa83.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4138daad7ec2b5cf5187179955ceda",
+    "url": "/alzheimer-lab/index.html"
+  },
+  {
+    "revision": "cc6b4188817c93b56475",
+    "url": "/alzheimer-lab/static/css/main.7034ab9c.chunk.css"
+  },
+  {
+    "revision": "512cf735e64b076a123c",
+    "url": "/alzheimer-lab/static/js/2.18c4e460.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alzheimer-lab/static/js/2.18c4e460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6b4188817c93b56475",
+    "url": "/alzheimer-lab/static/js/main.e1c246da.chunk.js"
+  },
+  {
+    "revision": "4559d7d8c63eae1ce7b8",
+    "url": "/alzheimer-lab/static/js/runtime-main.a2a021f1.js"
+  },
+  {
+    "revision": "ceb7f85526bd8ad94e4f30bec0597dc3",
+    "url": "/alzheimer-lab/static/media/delete.ceb7f855.png"
+  },
+  {
+    "revision": "372dbf44dd0b0bff30e07469f9bbb95a",
+    "url": "/alzheimer-lab/static/media/endView.372dbf44.jpeg"
+  },
+  {
+    "revision": "23759836fdfd617126176108a9949c23",
+    "url": "/alzheimer-lab/static/media/multi.23759836.jpeg"
+  },
+  {
+    "revision": "e3682b4d66f70c1d129c444cc6ac3cae",
+    "url": "/alzheimer-lab/static/media/open_screen.e3682b4d.png"
+  },
+  {
+    "revision": "a92cd407581d6dbfe07d781b0485e651",
+    "url": "/alzheimer-lab/static/media/three.a92cd407.jpeg"
+  }
+]);
